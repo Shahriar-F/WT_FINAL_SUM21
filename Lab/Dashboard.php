f4230878a75b23b5a9ad622548ae03da60ae9cb1
Shahriar <shahriarfahim20@gmail.com>
2021-07-18 16:50:13 +0600

    lab

---
+++ Lab/Dashboard.php
@@ -0,0 +1,15 @@
+<html>
+ <head></head>
+ <body>
+  <form>
+   <a href="alldepartments.php">
+   <input type="button" name="all_departments" value="All Departments"> </a>
+   
+   <a href="allstudents.php">
+   <input type="button" name="all_students" value="All Students"> </a>
+   
+   <a href="add_student.php">
+   <input type="button" name="add_student" value="Add Student"> </a>
+  </form>
+ </body>
+</html>
