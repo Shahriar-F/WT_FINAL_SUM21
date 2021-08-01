17830f07a005d0693bf4216f6c13aaeb28c5d9f2
Shahriar <shahriarfahim20@gmail.com>
2021-08-01 15:29:30 +0600

    ajax lab

---
+++ Ajax/index.php
@@ -0,0 +1,19 @@
+<script>
+  function get(id){
+	  return document.getElementById(id);
+  }
+  
+  function loadDoc(){
+	  
+	  var xhr = new XMLHttpRequest();
+	  xhr.open("GET","my_profile.php",true);
+	  xhr.onreaadystatechange=function(){
+	   if()this.reaadystate == 4 && this.status == 200){
+		    get("demo").innerHTML = this.responseText;
+	    }
+	  };
+	  xhr.send();
+  }
+</script>
+<button onclick="loadDoc()">Click me</button>
+<div id="demo"></div>
---
+++ Ajax/my_profile.php
@@ -0,0 +1,12 @@
+<html>
+ <head></head>
+ <?php $name = "Shahriar";?>
+ <body>
+  <h1 align="center"> MY profile</h1>
+  <table align="center">
+   <tr>
+    <td><?php echo $name;?></td>
+   </tr>
+  </table>
+ </body>
+</html>
